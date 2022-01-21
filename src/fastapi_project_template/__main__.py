e60f6b1033f3f5e4e4cad55a01763dc8e965b101
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2022-01-21 17:55:28 +0200

    some fixes, added example of extmod function call

--- src/fastapi_project_template/__main__.py
+++ src/fastapi_project_template/__main__.py
@@ -2,5 +2,7 @@ import uvicorn
 
 from fastapi_project_template.api.http import app
 from fastapi_project_template.conf import settings
+from fastapi_project_template.extmod import c_fib
 
+print(f'{c_fib(10)=}')
 uvicorn.run(app, host=settings.service_addr, port=settings.service_port)
