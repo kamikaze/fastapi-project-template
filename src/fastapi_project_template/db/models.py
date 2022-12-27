9605120aea3ad1c3886ed661610a663b63677dcc
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2022-12-28 00:45:15 +0200

    added user groups and password timestamp

--- src/fastapi_project_template/db/models.py
+++ src/fastapi_project_template/db/models.py
@@ -1,6 +1,7 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
-from sqlalchemy import Column, String, DateTime, ForeignKey, BIGINT
+from sqlalchemy import Column, String, DateTime, ForeignKey, BIGINT, UniqueConstraint
 from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import expression
 from sqlalchemy.sql.ddl import CreateColumn
 
@@ -25,15 +26,28 @@ def use_identity(element, compiler, **kw):
 
 class BaseDBModel:
     id = Column(BIGINT, primary_key=True)
-    created_at = Column(DateTime, server_default=UTCNow())
-    updated_at = Column(DateTime, onupdate=UTCNow())
+    created_at = Column(DateTime(timezone=True), server_default=UTCNow())
+    updated_at = Column(DateTime(timezone=True), onupdate=UTCNow())
 
 
-class UserGroup(BaseDBModel, Base):
-    __tablename__ = 'user_groups'
+class Group(BaseDBModel, Base):
+    __tablename__ = 'groups'
 
     name = Column(String, nullable=False)
 
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    group_id = Column(BIGINT, ForeignKey('user_groups.id'), nullable=True)
+    __tablename__ = 'users'
+
+    password_changed_at = Column(DateTime(timezone=True))
+
+
+class UserGroup(BaseDBModel, Base):
+    __tablename__ = 'user_groups'
+
+    user_id = Column(UUID, ForeignKey('users.id'), nullable=False)
+    group_id = Column(BIGINT, ForeignKey('groups.id'), nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'group_id', name='uq_user_groups'),
+    )
