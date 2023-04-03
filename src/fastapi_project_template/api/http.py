f8116c5cf5172757cc92837ad7e8b749f97609ef
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2023-04-03 16:27:33 +0300

    dependency bump, replacing common functions with python3-commons

--- src/fastapi_project_template/api/http.py
+++ src/fastapi_project_template/api/http.py
@@ -4,11 +4,12 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
+from python3_commons.db import connect_to_db
 from starlette.applications import Starlette
 
 from fastapi_project_template.api.v1.endpoints import router, auth_router, users_router
+from fastapi_project_template.conf import settings
 from fastapi_project_template.db import database
-from fastapi_project_template.helpers import connect_to_db
 
 logger = logging.getLogger(__name__)
 origins = [
@@ -21,7 +22,7 @@ origins = [
 
 @asynccontextmanager
 async def lifespan(app: Starlette):
-    await connect_to_db(database)
+    await connect_to_db(database, settings.db_dsn)
 
     yield
 
--- src/fastapi_project_template/helpers.py
+++ src/fastapi_project_template/helpers.py
@@ -1,34 +1,10 @@
-import asyncio
-import datetime
 import logging
 import os
 
 from alembic import command
 from alembic.config import Config
-from asyncpg import CannotConnectNowError
-
-from fastapi_project_template.conf import settings
 
 logger = logging.getLogger(__name__)
-
-
-def date_from_string(string: str, fmt: str = '%d.%m.%Y') -> datetime.date:
-    try:
-        return datetime.datetime.strptime(string, fmt).date()
-    except ValueError:
-        return datetime.date.fromisoformat(string)
-
-
-def datetime_from_string(string: str) -> datetime.datetime:
-    try:
-        return datetime.datetime.strptime(string, '%d.%m.%Y %H:%M:%S')
-    except ValueError:
-        return datetime.datetime.fromisoformat(string)
-
-
-def date_range(start_date, end_date):
-    for n in range(int((end_date - start_date).days + 1)):
-        yield start_date + datetime.timedelta(days=n)
 
 
 def run_db_migrations(config, dsn: str, script_location: str) -> None:
@@ -40,40 +16,3 @@ def run_db_migrations(config, dsn: str, script_location: str) -> None:
     alembic_cfg.set_main_option('sqlalchemy.url', dsn)
     command.upgrade(alembic_cfg, 'head')
     os.chdir(original_wd)
-
-
-async def connect_to_db(database):
-    logger.info('Waiting for services')
-    logger.debug(f'DB_DSN: {settings.db_dsn}')
-    timeout = 0.001
-    total_timeout = 0
-
-    for i in range(15):
-        try:
-            await database.connect()
-        except (ConnectionRefusedError, CannotConnectNowError):
-            timeout *= 2
-            await asyncio.sleep(timeout)
-            total_timeout += timeout
-        else:
-            break
-    else:
-        msg = f'Unable to connect database for {int(total_timeout)}s'
-        logger.error(msg)
-        raise ConnectionRefusedError(msg)
-
-
-def tries(times):
-    def func_wrapper(f):
-        async def wrapper(*args, **kwargs):
-            for time in range(times if times > 0 else 1):
-                # noinspection PyBroadException
-                try:
-                    return await f(*args, **kwargs)
-                except Exception as exc:
-                    if time >= times:
-                        raise exc
-
-        return wrapper
-
-    return func_wrapper
