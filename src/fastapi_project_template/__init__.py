e262ec47ef9b880e503e7fdf84e1fb53a26d2484
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2024-09-07 01:59:50 +0300

    Fixed dependencies

--- src/fastapi_project_template/__init__.py
+++ src/fastapi_project_template/__init__.py
@@ -1,10 +1,9 @@
-from pkg_resources import DistributionNotFound, get_distribution
+import importlib.metadata
 
 try:
-    # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = importlib.metadata.version(dist_name)
+except importlib.metadata.PackageNotFoundError:
     __version__ = 'unknown'
 finally:
-    del get_distribution, DistributionNotFound
+    del importlib.metadata
