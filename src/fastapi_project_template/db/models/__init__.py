2c72a1f731c33dddc72c1ec169967caee9e8fad5
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-09-04 20:10:05 +0300

    Fixing unused (but needed) imports

--- src/fastapi_project_template/db/models/__init__.py
+++ src/fastapi_project_template/db/models/__init__.py
@@ -1,9 +1,9 @@
 from python3_commons.db import Base as Base
-from python3_commons.db.models.auth import ApiKey as ApiKey, User as User, UserGroup as UserGroup
-from python3_commons.db.models.rbac import (
-    RBACApiKeyRole as RBACApiKeyRole,
-    RBACPermission as RBACPermission,
-    RBACRole as RBACRole,
-    RBACRolePermission as RBACRolePermission,
-    RBACUserRole as RBACUserRole,
-)
+from python3_commons.db.models.auth import ApiKey as ApiKey
+from python3_commons.db.models.auth import User as User
+from python3_commons.db.models.auth import UserGroup as UserGroup
+from python3_commons.db.models.rbac import RBACApiKeyRole as RBACApiKeyRole
+from python3_commons.db.models.rbac import RBACPermission as RBACPermission
+from python3_commons.db.models.rbac import RBACRole as RBACRole
+from python3_commons.db.models.rbac import RBACRolePermission as RBACRolePermission
+from python3_commons.db.models.rbac import RBACUserRole as RBACUserRole
