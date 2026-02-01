6190f238418fc19d73892d50bce69b317e586e8d
Olegs Korsaks <kamikaze.is.waiting.you@gmail.com>
2026-02-02 00:31:36 +0200

    Adjusted compose file

---
+++ .docker/keycloak/data/import/keycloak-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+H2_DIR="/opt/keycloak/data/h2"
+IMPORT_DIR="/opt/keycloak/data/import"
+
+echo "Checking for H2 database files..."
+
+if [ ! -f "$H2_DIR/keycloakdb.mv.db" ]; then
+    mkdir -p $H2_DIR
+    echo "No existing H2 DB. Copying from import directory..."
+    cp -r "$IMPORT_DIR/"* "$H2_DIR/"
+else
+    echo "H2 DB already exists, skipping copy."
+fi
+
+echo "Starting Keycloak..."
+exec /opt/keycloak/bin/kc.sh start-dev
