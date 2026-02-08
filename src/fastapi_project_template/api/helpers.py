70e4c4fa26c037dd24a3692edcc8a07baaec137d
Olegs Korsaks <kamikaze.is.waiting.you@gmail.com>
2026-02-08 18:05:48 +0200

    Code fixes

--- src/fastapi_project_template/api/helpers.py
+++ src/fastapi_project_template/api/helpers.py
@@ -3,8 +3,8 @@ from typing import Annotated
 from fastapi import Header, Request
 
 
-def get_client_ip(request: Request, x_forwarded_for: Annotated[str | None, Header()] = None) -> str:
+def get_client_ip(request: Request, x_forwarded_for: Annotated[str | None, Header()] = None) -> str | None:
     if x_forwarded_for:
         return x_forwarded_for
 
-    return request.client.host
+    return client.host if (client := request.client) else None
