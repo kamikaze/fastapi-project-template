f47c468a3e4afc6a45dc33ecd95a953bac5dfbb5
Olegs Korsaks <kamikaze.is.waiting.you@gmail.com>
2026-05-08 19:37:20 +0300

    dependency bump

--- .docker/rabbitmq/init.sh
+++ .docker/rabbitmq/init.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -euo pipefail
 
 rabbitmq-server &
 
 echo "Waiting for RabbitMQ to be fully started..."
 
-# ✅ Correct readiness check
 until rabbitmqctl await_startup
 do
   sleep 1
